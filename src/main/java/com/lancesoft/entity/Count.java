9a00365faa447d963d82a73a69e3735cb5e86371
Swathi-Java <120640493+Swathi-Java@users.noreply.github.com>
2023-04-24 14:43:45 +0530

    Update Count.java

--- src/main/java/com/lancesoft/entity/Count.java
+++ src/main/java/com/lancesoft/entity/Count.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Count {
+  //>>>>>>>>>>>>>>>>>>>>>>>>>>>
 private int employecount;
 private int projectcount;
 }
